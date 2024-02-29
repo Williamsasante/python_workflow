68f67e43e4f32bd8b29b18776bd29fe5611d50de
William Amoako Asante <a_williams55@yahoo.com>
2024-02-29 16:00:47 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+# my = "i am"
+# age = "18"
+
+# statementz = my+" "+ age
+# print(statementz.title())
+
+
+# the_boolean = 0 < 10
+# print(the_boolean)
+
+# var = 6.36277
+# print(type(var))
+
+# me = 10 > 100
+
+# print(me)
+
+
+
+# birth_year = input('birthyear:')
+
+# age = 2024 - int(birth_year)
+
+# print(age)
+
+
+# weight_in_kg = input('Weight in KG:')
+
+# weight_in_pounds = 0.45359237 * int(weight_in_kg)
+
+# print(weight_in_pounds)
+
+
+
+
+
+#using loops to find keys and values
+# numbers = {"First": '1', "Second":'2'}
+# for key, value in numbers.items():
+#     print(F"{key} has a value of {value}")
+
+
+#using normal variable declarations 
+# name = "Kwame"
+# age = 19
+# location = "London"
+# print(F"His name is {name} and he is {age} years old")
+# print(F"Suprisingly, he lives in {location}")
+
+
+
+
+name = ['John','Kofi','Michael', 'Brad']
+
+name.insert(0, 'Kwame')
+name.insert(1, 'Kweku') 
+
+print (name)
+
