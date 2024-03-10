80dd2a9de96069b36aa0a5a96f6b8e68702e8c90
William Amoako Asante <a_williams55@yahoo.com>
2024-03-10 00:55:25 +0000

    on python

--- arithmetic.py
+++ arithmetic.py
@@ -4,3 +4,12 @@ c=7
 
 print(a is b)
 print(c is not a)
+
+print("--------------------------------")
+
+numbers = [1, 2, 3, 4, 5]
+print(2 in numbers)
+
+print("--------------------------------")
+colors = ["red", "blue", "green", "yellow"]
+print("red" in colors)
